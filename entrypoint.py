9be0d1d41a596d97a69a5c466fdc0fe0989b62f7
David Butler <croepha@gmail.com>
2016-03-09 11:04:18 -0600

    initial prototype

--- entrypoint.py
+++ entrypoint.py
@@ -7,9 +7,16 @@ PGPORT = os.environ.get('PGPORT', '5432')
 PGROLE = os.environ.get('PGROLE', 'postgres')
 PGDATABASE = os.environ.get('PGDATABASE', 'postgres')
 PGPASS = os.environ.get('PGPASS', '')
-
 ZBXHOST = os.environ.get('ZBXHOST', 'zabbix')
 ZBXPORT = os.environ.get('ZBXPORT', '')
+
+DEBUG = os.environ.get('DEBUG', '')
+
+if DEBUG.upper().strip() in ('Y', 'ON', 'YES', '1'):
+    DEBUG=True
+else:
+    DEBUG=False
+
 
 if ZBXPORT:
     ZBXPORT = ':' + ZBXPORT
@@ -17,7 +24,7 @@ if ZBXPORT:
 
 PGPASSFILE = os.environ.get('PGPASSFILE', '/usr/local/etc/pgpass')
 os.environ["PGPASSFILE"] = PGPASSFILE
-pgsql_conf = '/usr/local/etc/pgsql_func.conf'
+pgsql_conf = '/usr/local/etc/pgsql_funcs.conf'
 zbx_conf = '/etc/zabbix/zabbix_agentd.conf'
 
 pgsql_conf_template = """
@@ -53,6 +60,8 @@ with open(zbx_conf, 'w') as f:
     f.write('AllowRoot=1\n')
     f.write('LogType=console\n')
     f.write('Server=%s\n' % ZBXHOST)
+    if DEBUG:
+        f.write('DebugLevel=4')
 
 os.makedirs('/var/run/zabbix', exist_ok=True)
 
