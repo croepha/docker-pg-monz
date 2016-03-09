5ad9fa4d6a48afcf169382ef2f3bee8afbd340d3
David Butler <croepha@gmail.com>
2016-03-09 08:30:11 -0600

    initial prototype

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM ubuntu
 
-RUN \
-  apt-get install -y zabbix-agent postgresql-client
+RUN apt-get install -y  wget postgresql-client \
+  && wget http://repo.zabbix.com/zabbix/3.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.0-1+trusty_all.deb \
+  && dpkg -i zabbix-release_3.0-1+trusty_all.deb \
+  && rm -v zabbix-release_3.0-1+trusty_all.deb \
+  && apt-get update \
+  && apt-get install -y zabbix-agent zabbix-sender
 
 COPY pg_monz/pg_monz/usr-local-bin/*  /usr/local/bin/
 COPY pg_monz/pg_monz/zabbix_agentd.d/userparameter_pgsql.conf /etc/zabbix/zabbix_agentd.conf.d/pg_monz.conf
--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,53 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 
+PGHOST = os.environ.get('PGHOST', 'postgres')
+PGPORT = os.environ.get('PGPORT', '5432')
+PGROLE = os.environ.get('PGROLE', 'postgres')
+PGDATABASE = os.environ.get('PGDATABASE', 'postgres')
+PGPASS = os.environ.get('PGPASS', '')
+
+ZBXHOST = os.environ.get('ZBXHOST', 'zabbix')
+ZBXPORT = os.environ.get('ZBXPORT', '')
+
+if ZBXPORT:
+    ZBXPORT = ':' + ZBXPORT
+
+
+PGPASSFILE = os.environ.get('PGPASSFILE', '/usr/local/etc/pgpass')
+os.environ["PGPASSFILE"] = PGPASSFILE
+pgsql_conf = '/usr/local/etc/pgsql_func.conf'
+zbx_conf = '/etc/zabbix/zabbix_agentd.conf'
+
+pgsql_conf_template = """
+PGHOST=%s
+PGPORT=%s
+PGROLE=%s
+PGDATABASE=%s
+"""
+
+if PGPASS:
+    with open(PGPASSFILE, 'w') as f:
+        f.write('%s:%s:%s:%s:%s\n' % (
+            PGHOST, PGPORT, PGDATABASE, PGROLE, PGPASS
+        ))
+
+with open(pgsql_conf, 'w') as f:
+    f.write(pgsql_conf_template % (
+        PGHOST, PGPORT, PGROLE, PGDATABASE
+    ))
+
+with open(zbx_conf, 'r') as f:
+    zbx_conf_data = f.readlines()
+
+with open(zbx_conf, 'w') as f:
+    for line in zbx_conf_data:
+        if not line.strip().startswith(["Hostname=", "ServerActive="]):
+            f.write(line)
+        f.write('ServerActive=%s%s\n' % (ZBXHOST, ZBXPORT))
+        f.write('AllowRoot=1\n')
+
+os.makedirs('/var/run/zabbix', exist_ok=True)
+
+os.execlp()
